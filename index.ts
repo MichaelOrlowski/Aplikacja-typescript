b0dcb83cac5034efde25e9bb03e64f9aa71af7f8
michal.orlowski <michal.orlowski.95@gmail.com>
2021-03-07 14:22:27 +0100

    Basic HTML setup

--- index.ts
+++ index.ts
@@ -3,6 +3,59 @@ const secondtdata = document.getElementById("data2");
 const thirddata = document.getElementById("data3");
 const fourthdata = document.getElementById("data4");
 
-class startapp{
+
+
+const sum = document.getElementById("sum");
+function suma(x:number, y:number){
+  sum.innerText = String(x);
+}
+
+
+const button = document.getElementsByClassName("suma");
+button[0].addEventListener("click", function(){
+  sum.innerText = firstdata.innerText;
+  console.log(firstdata.innerText);
+})
+console.log(firstdata.innerText);
+
+firstdata.addEventListener("change", function(){
+  sum.innerText = firstdata.innerText;
+})
+
+
+
+var message:string = "Hello World"
+console.log(message)
+
+class Stats{
+  data1;
+  data2;
+  data3;
+  data4;
+
+  min;
+  max;
+  avg;
+  sum;
+
+  constructor() {
+    this.getElements();
+    // this.bindEvents();
+  }
+
+  getElements() {
+    this.data1.value = +document.getElementById("data1").innerText;
+    console.log(this.data1);
+  }
+
+  button = document.getElementById("suma");
+
+
+
+  buttonclick(){
+    console.log(this.data1);
+  }
 
 }
+
+new Stats;

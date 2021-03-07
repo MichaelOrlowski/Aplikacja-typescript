c4534eb3d03459e4b49efd9d72b53a08f3aa76ce
michal.orlowski <michal.orlowski.95@gmail.com>
2021-03-07 13:39:56 +0100

    Basic HTML setup

---
+++ index.ts
@@ -0,0 +1,8 @@
+const firstdata = document.getElementById("data1");
+const secondtdata = document.getElementById("data2");
+const thirddata = document.getElementById("data3");
+const fourthdata = document.getElementById("data4");
+
+class startapp{
+
+}
